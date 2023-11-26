9b984e6cb921a722ab21061ddadeca2553e51d83
Mélanie <melan60@free.fr>
2023-11-26 16:12:32 +0100

    added VideoScramble project

---
+++ VideoScramble/src/main/java/video/VideoGrabDemo.java
@@ -0,0 +1,62 @@
+package video;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+
+
+import javafx.stage.WindowEvent;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.core.Core;
+import video.VideoGrabDemoController;
+
+import java.io.InputStream;
+
+
+public class VideoGrabDemo extends Application {
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //VideoCapture capture = new VideoCapture(0); // 0 indique la première webcam
+        //Mat frame = new Mat();
+
+        try {
+            // load the FXML resource
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("VideoGrabDemo.fxml"));
+            // store the root element so that the controllers can use it
+            BorderPane rootElement = (BorderPane) loader.load();
+
+
+            Scene scene = new Scene(rootElement, 1200, 800);
+
+            primaryStage.setTitle("Capture vidéo de la webcam avec OpenCV");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            // set the proper behavior on closing the application
+            VideoGrabDemoController controller = loader.getController();
+            primaryStage.setOnCloseRequest((new EventHandler<WindowEvent>() {
+                public void handle(WindowEvent we) {
+                    controller.setClosed();
+                }
+            }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
---
+++ VideoScramble/src/main/java/video/VideoGrabDemoController.java
@@ -0,0 +1,269 @@
+package video;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.embed.swing.SwingFXUtils;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * The controller for our application, where the application logic is
+ * implemented. It handles the button for starting/stopping the camera and the
+ * acquired video stream.
+ *
+ * @author <a href="mailto:luigi.derussis@polito.it">Luigi De Russis</a>
+ * @author <a href="http://max-z.de">Maximilian Zuleger</a> (minor fixes)
+ * @version 2.0 (2016-09-17)
+ * @since 1.0 (2013-10-20)
+ *
+ */
+public class VideoGrabDemoController
+{
+    // the FXML button
+    @FXML
+    private Button button;
+    // the FXML image view
+    @FXML
+    private ImageView currentFrame;
+
+    // a timer for acquiring the video stream
+    private ScheduledExecutorService timer;
+    // the OpenCV object that realizes the video capture
+    private VideoCapture capture = new VideoCapture();
+    // a flag to change the button behavior
+    private boolean cameraActive = false;
+    // the id of the camera to be used
+    // when using apple OS with an associated iphone nearby, 0 will be iphone's cam
+    private static int cameraId = 0;
+
+    /**
+     * The action triggered by pushing the button on the GUI
+     *
+     * @param event
+     *            the push button event
+     */
+    @FXML
+    protected void startCamera(ActionEvent event)
+    {
+
+        if (!this.cameraActive)
+        {
+            // start the video capture
+            this.capture.open(cameraId);
+
+            // is the video stream available?
+            if (this.capture.isOpened())
+            {
+                this.cameraActive = true;
+
+                // grab a frame every 33 ms (30 frames/sec)
+                Runnable frameGrabber = new Runnable() {
+
+                    @Override
+                    public void run()
+                    {
+                        // effectively grab and process a single frame
+                        // note : macbook & iphone 11 : 1080p
+                        Mat frame = grabFrame();
+                        // more complex image processing can be called from here 
+                        // convert and show the frame
+                        Image imageToShow = mat2Image(frame);
+                        updateImageView(currentFrame, imageToShow);
+                        currentFrame.setFitWidth(800);
+                        currentFrame.setPreserveRatio(true);
+
+                    }
+                };
+
+                this.timer = Executors.newSingleThreadScheduledExecutor();
+                this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+
+                // update the button content
+                this.button.setText("Stop Camera");
+            }
+            else
+            {
+                // log the error
+                System.err.println("Impossible to open the camera connection...");
+            }
+        }
+        else
+        {
+            // the camera is not active at this point
+            this.cameraActive = false;
+            // update again the button content
+            this.button.setText("Start Camera");
+
+            // stop the timer
+            this.stopAcquisition();
+        }
+    }
+
+    /**
+     * Get a frame from the opened video stream (if any)
+     *
+     * @return the {@link Mat} to show
+     */
+    private Mat grabFrame()
+    {
+        // init everything
+        Mat frame = new Mat();
+
+        // check if the capture is open
+        if (this.capture.isOpened())
+        {
+            try
+            {
+                // read the current frame
+                this.capture.read(frame);
+
+                // if the frame is not empty, process it
+                if (!frame.empty())
+                {
+                    // basic single frame processing can be performed here
+                    // TODO ?
+                }
+
+            }
+            catch (Exception e)
+            {
+                // log the error
+                System.err.println("Exception during the image elaboration: " + e);
+            }
+        }
+
+        return frame;
+    }
+
+    /**
+     * Stop the acquisition from the camera and release all the resources
+     */
+    private void stopAcquisition()
+    {
+        if (this.timer!=null && !this.timer.isShutdown())
+        {
+            try
+            {
+                // stop the timer
+                this.timer.shutdown();
+                this.timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+            }
+            catch (InterruptedException e)
+            {
+                // log any exception
+                System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+            }
+        }
+
+        if (this.capture.isOpened())
+        {
+            // release the camera
+            this.capture.release();
+        }
+    }
+
+    /**
+     * Update the {@link ImageView} in the JavaFX main thread
+     *
+     * @param view
+     *            the {@link ImageView} to update
+     * @param image
+     *            the {@link Image} to show
+     */
+    private void updateImageView(ImageView view, Image image)
+    {
+        onFXThread(view.imageProperty(), image);
+    }
+
+    /**
+     * On application close, stop the acquisition from the camera
+     */
+    protected void setClosed()
+    {
+        this.stopAcquisition();
+    }
+
+    private Image matToJavaFXImage(Mat mat) {
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".png", mat, buffer);
+        return new Image(new java.io.ByteArrayInputStream(buffer.toArray()));
+    }
+
+
+    /**
+     * Convert a Mat object (OpenCV) in the corresponding Image for JavaFX
+     *
+     * @param frame
+     *            the {@link Mat} representing the current frame
+     * @return the {@link Image} to show
+     */
+    public static Image mat2Image(Mat frame)
+    {
+        try
+        {
+            return SwingFXUtils.toFXImage(matToBufferedImage(frame), null);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Cannot convert the Mat obejct: " + e);
+            return null;
+        }
+    }
+
+    /**
+     * @param original
+     *            the {@link Mat} object in BGR or grayscale
+     * @return the corresponding {@link BufferedImage}
+     */
+    private static BufferedImage matToBufferedImage(Mat original)
+    {
+        // init
+        BufferedImage image = null;
+        int width = original.width(), height = original.height(), channels = original.channels();
+        byte[] sourcePixels = new byte[width * height * channels];
+        original.get(0, 0, sourcePixels);
+
+        if (original.channels() > 1)
+        {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        }
+        else
+        {
+            image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        }
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
+
+        return image;
+    }
+
+    /**
+     * Generic method for putting element running on a non-JavaFX thread on the
+     * JavaFX thread, to properly update the UI
+     *
+     * @param property
+     *            a {@link ObjectProperty}
+     * @param value
+     *            the value to set for the given {@link ObjectProperty}
+     */
+    public static <T> void onFXThread(final ObjectProperty<T> property, final T value)
+    {
+        Platform.runLater(() -> {
+            property.set(value);
+        });
+    }
+
+}
